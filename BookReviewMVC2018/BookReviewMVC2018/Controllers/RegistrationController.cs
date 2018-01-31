0cc4c90f3924254853d81953a940dbaddab865c6
craigbpeterson <craig.b.peterson@gmail.com>
2018-01-31 15:22:12 -0800

    01-31-2018 A4: Registration

---
+++ BookReviewMVC2018/BookReviewMVC2018/Controllers/RegistrationController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookReviewMVC2018.Models;
+
+namespace BookReviewMVC2018.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        BookReviewDbEntities db = new BookReviewDbEntities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.Reviewers.ToList()); //listing reviewiers allows us to see if the registration is working
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "reviewerUserName,reviewerFirstName,reviewerLastName,reviewerEmail,reviewPlainPassword")]Reviewer r)
+        {
+            int result = db.usp_NewReviewer(
+                r.ReviewerUserName, 
+                r.ReviewerFirstName, 
+                r.ReviewerLastName, 
+                r.ReviewerEmail, 
+                r.ReviewPlainPassword //this one is Review instead of Reviewer only because Steve made a mistake in the BookReview db
+            );
+
+            if(result != -1) //result would be -1 if previous block failed
+            {
+                return RedirectToAction("Index"); //for community assist, create a new view called ThankYou
+            }
+
+            return View(); //this line only runs when the previous if statement doesn't evaluate true
+        }
+    }
+}
---
+++ CommunityAssistMVC/CommunityAssistMVC/Controllers/RegistrationController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistMVC.Models;
+
+namespace CommunityAssistMVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.People.ToList()); //listing people allows us to see if the registration is working
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }  
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "LastName, FirstName, Email, Password, Apartment, Street, City, State, Zipcode, Phone")]NewPerson p)
+        {
+            int result = db.usp_Register(
+                p.LastName, 
+                p.FirstName, 
+                p.Email, 
+                p.PlainPassword, 
+                p.Apartment, 
+                p.Street, 
+                p.City, 
+                p.State, 
+                p.Zipcode, 
+                p.Phone
+            );
+
+            Message m = new Message();
+
+            if (result != -1) //result would be -1 if previous block failed
+            {
+                m.MessageText = "Thank You for Registering!";
+                return RedirectToAction("Result",m);
+            }
+
+            m.MessageText = "Oops. Something went wrong. Registration unsuccessful.";
+            return RedirectToAction("Result",m); //this line only runs when the previous if statement doesn't evaluate true
+        }
+
+    }
+}
