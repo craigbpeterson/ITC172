689f41d804b82b9eabd8a74341109e5bddaec290
craigbpeterson <craig.b.peterson@gmail.com>
2018-02-21 12:27:10 -0800

    02-21-2018 A7: Application for Assistance

---
+++ BookReviewMVC2018/BookReviewMVC2018/Controllers/NewReviewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookReviewMVC2018.Models;
+
+namespace BookReviewMVC2018.Controllers
+{
+    public class NewReviewController : Controller
+    {
+        BookReviewDbEntities db = new BookReviewDbEntities(); //instantiate the database model
+        // GET: NewReview
+        public ActionResult Index()
+        {
+            if(Session["reviewerKey"]==null) //check to make sure the user is logged in
+            {
+                Message m = new Message();
+                m.MessageText = "You must be logged in to add a review.";
+                return RedirectToAction("Result", m);
+            }
+
+            ViewBag.BookKey = new SelectList(db.Books, "BookKey", "BookTitle"); //if they are logged in, pass these two columns from the database to the view using a new ViewBag named BookKey
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "BookKey, ReviewerKey, ReviewDate, ReviewTitle, ReviewRating, ReviewText")]Review r)
+        {
+            r.ReviewerKey = (int)Session["reviewerKey"];
+            r.ReviewDate = DateTime.Now;
+            db.Reviews.Add(r);
+            db.SaveChanges();
+
+            Message m = new Message();
+            m.MessageText = "Thank you for your review!";
+            return View("Result", m);
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+    }
+}
---
+++ CommunityAssistMVC/CommunityAssistMVC/Controllers/NewGrantAppController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistMVC.Models;
+
+namespace CommunityAssistMVC.Controllers
+{
+    public class NewGrantAppController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: NewGrantApp
+        public ActionResult Index()
+        {
+            if(Session["personkey"] == null)
+            {
+                Message m = new Message();
+                m.MessageText = "You must be logged in to apply for a grant.";
+                return RedirectToAction("Result", m);
+            }
+
+            ViewBag.GrantTypeKey = new SelectList(db.GrantTypes, "GrantTypeKey", "GrantTypeName");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "GrantApplicationKey, PersonKey, GrantAppicationDate, GrantTypeKey, GrantTypeName, GrantApplicationRequestAmount, GrantApplicationReason, GrantApplicationStatusKey, GrantApplicationAllocationAmount")]GrantApplication g)
+        {
+            g.PersonKey = (int)Session["personkey"];
+            g.GrantAppicationDate = DateTime.Now;
+            g.GrantApplicationStatusKey = 1;
+            db.GrantApplications.Add(g);
+            db.SaveChanges();
+
+            Message m = new Message();
+            m.MessageText = "Thank you for applying for a grant.";
+            return View("Result", m);
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+    }
+}
